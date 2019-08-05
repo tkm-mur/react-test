e7b27be4be930b85072f29db26fbf8c88f6c531b
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2019-08-05 15:19:59 +0900

    update: HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import React, {FC} from "react";
 import {Container} from "semantic-ui-react";
-import CharacterList, {Character} from "./CharacterList";
+import Hoc from './Hoc'
 
 const App: FC<{}> = () => {
-  const characters: Character[] = [
-    {
-      id: 1,
-      name: 'test1',
-      age: 20,
-      height: 170
-    },
-    {
-      id: 2,
-      name: 'test2',
-      age: 23,
-      height: 162
-    },
-    {
-      id: 3,
-      name: 'test3',
-      age: 30,
-      height: 182
-    }
-  ];
-
-  const CharacterListProps = {
-    school: 'TEST',
-    characters: characters
-  };
-
   return (
     <Container>
-      <CharacterList {...CharacterListProps} />
+      <Hoc/>
     </Container>
   )
 };
